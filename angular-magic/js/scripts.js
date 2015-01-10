c0d03afec183e1314ee4d43f1255b0901126fd67
Nicolaj Nyvang <nicolajnyvang@gmail.com>
2015-01-10 23:47:34 +0100

    First step - now with comments
    Epic! I forgot to save the scripts.js file :-1:
    Well, here it is then

--- angular-magic/js/scripts.js
+++ angular-magic/js/scripts.js
@@ -1,11 +1,35 @@
 (function(){ // Begin closure
 	'use strict';
+	// We start out by bootstrapping AngularJS itself, which is initiated after the DOM is ready
 	var app = angular.module('ngBootstrap', [ ]);
 
+	/**
+	 * Then we define our first directive, cooresponding to the 
+	 * outer container in Bootstrap, which of course is the 'container'
+	 */
   app.directive('container', function () {
     return {
-    	transclude: true,
+    	/**
+    	 * The transclude is the magic keyword here.
+			 * Without it, our Directive element will not act like a wrapper 
+			 * and will not contain anything...
+    	 */
+    	transclude: true, // <-- Important 
+    	
+    	/**
+    	 * Then we define the template we wish to use whenever the 
+    	 * <container> element is encountered by Angular
+    	 */
       templateUrl: 'angular-magic/templates/container.html',
+
+      /**
+       * The 'E' in restrict tells Angular that we allow only
+       * 'Elements' to use this directive.
+       * We could also have use 'A' which stands for 'Attribute'.
+       * This, however, would have gotten os nowhere, because we´d still 
+       * have to write the div-tag - just with an attribute instead of 
+       * the class reference (class="container") - like this: <div container></div
+       */
       restrict: 'E'
     };
 	});
